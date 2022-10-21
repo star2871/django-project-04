aa903fd00cbf5512731f3e4eb7f80eb5b568e757
star2871 <star2871@gmail.com>
2022-10-21 16:15:30 +0900

    리뷰 정보 수정기능 완료

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "reviews"
 
 urlpatterns = [
-    path("create/", views.create, name="create"),
     path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("<int:pk>", views.detail, name="detail"),
+    path("<int:pk>/update/", views.update, name="update"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -30,3 +30,32 @@ def create(request):
         "review_form": review_form,
     }
     return render(request, "reviews/new.html", context=context)
+
+def detail(request, pk):
+    # 특정 글을 가져온다.
+    review = Review.objects.get(pk=pk)
+    # template에 객체 전달
+    context = {
+        "review": review,
+    }
+    return render(request, "reviews/detail.html", context)
+
+def update(request, pk):
+    # GET : Form을 제공
+    review = Review.objects.get(pk=pk)
+    # review_form = ReviewForm(instance=review)
+    if request.method == "POST":
+        # POST : input 값 가져와서, 검증하고, DB에 저장
+        review_form = ReviewForm(request.POST, instance=review)
+        if review_form.is_valid():
+            # 유효성 검사 통과하면 저장하고, 상세보기 페이지로
+            review_form.save()
+            return redirect("reviews:detail", review.pk)
+        # 유효성 검사 통과하지 않으면 => context 부터해서 오류메시지 담긴 article_form을 랜더링
+    else:
+        # GET : Form을 제공
+        review_form = ReviewForm(instance=review)
+    context = {
+        "review_form": review_form,
+    }
+    return render(request, "reviews/update.html", context)
